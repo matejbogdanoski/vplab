494492dbb2a954ca48ae90a27082b4c315a98900
matejbogdanoski <37632172+matejbogdanoski@users.noreply.github.com>
2018-05-01 23:45:00 +0200

    Add files via upload

---
+++ VPLabPacman/VPLabPacman/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Resources;
+
+namespace VPLabPacman
+{
+    public partial class Form1 : Form
+    {
+        Timer timer;
+        Pacman pacman;
+        static readonly int TIME_INTERVAL = 250;
+        static readonly int WORLD_WIDTH =  15;
+        static readonly int WORLD_HEIGHT = 10;
+        Image foodImage;
+        bool[][] foodWorld;
+
+        public Form1()
+        {
+            InitializeComponent();
+            foodImage = VPLabPacman.Properties.Resources.food;
+            this.DoubleBuffered = true;
+            newGame();
+        }
+
+        public void newGame()
+        {
+            pacman = new Pacman();
+            this.Width = Pacman.RADIUS * 2 * (WORLD_WIDTH + 1);
+            this.Height = Pacman.RADIUS * 2 * (WORLD_HEIGHT + 1);
+
+            foodWorld = new bool[WORLD_HEIGHT][];
+            for(int i=0;i< WORLD_HEIGHT; i++)
+            {
+                foodWorld[i] = new bool[WORLD_WIDTH];
+            }
+            for(int i = 0; i < WORLD_HEIGHT; i++)
+            {
+                for(int j = 0; j < WORLD_WIDTH; j++)
+                {
+                    foodWorld[i][j] = true;
+                }
+            }
+            timer = new Timer();
+            timer.Interval = TIME_INTERVAL;
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            for (int i = 0; i < WORLD_HEIGHT; i++)
+            {
+                for (int j = 0; j < WORLD_WIDTH; j++)
+                {
+
+                    foodWorld[pacman.Y][pacman.X] = false;
+
+                }
+            }
+
+            pacman.dvizi(WORLD_WIDTH, WORLD_HEIGHT);
+
+            Invalidate();
+        }
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                pacman.nasoka = Pacman.Nasoka.gore;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                pacman.nasoka = Pacman.Nasoka.dole;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                pacman.nasoka = Pacman.Nasoka.levo;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                pacman.nasoka = Pacman.Nasoka.desno;
+            }
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            for (int i = 0; i < foodWorld.Length; i++)
+            {
+                for (int j = 0; j < foodWorld[i].Length; j++)
+                {
+                    if (foodWorld[i][j])
+                    {
+                        g.DrawImageUnscaled(foodImage, j * Pacman.RADIUS * 2 + (Pacman.RADIUS * 2 - foodImage.Height) / 2 +20, i * Pacman.RADIUS * 2 + (Pacman.RADIUS * 2 - foodImage.Width) / 2 +10);
+                    }
+                }
+            }
+            pacman.iscrtaj(g);
+        }
+    }
+}
---
+++ VPLabPacman/VPLabPacman/Pacman.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPLabPacman
+{
+    public class Pacman
+    {
+        public enum Nasoka { levo,gore,desno,dole }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Nasoka nasoka { get; set; }
+
+        public static int RADIUS = 20;
+        public static int Brzina = 20;
+        public bool otvorenaUsta { get; set; }
+        public Brush brush { get; set; }
+
+        public Pacman()
+        {
+            nasoka = Nasoka.desno;
+            otvorenaUsta = false;
+            brush = new SolidBrush(Color.Yellow);
+            X = 5;
+            Y = 7;
+        }
+
+        public void smeniNasoka(Nasoka n)
+        {
+            this.nasoka = n;
+        }
+
+        public void dvizi(int x, int y)
+        {
+            if(nasoka == Nasoka.desno)
+            {
+                if (X == x - 1)
+                {
+                    X = 0;
+                }
+                else X++;
+            }
+            if (nasoka == Nasoka.levo)
+            {
+                if (X == 0)
+                {
+                    X = x - 1;
+                }
+                else X--;
+            }
+            if (nasoka == Nasoka.dole)
+            {
+                if (Y == y - 1)
+                {
+                    Y = 0;
+                }
+                else Y++;
+            }
+            if (nasoka == Nasoka.gore)
+            {
+                if (Y == 0)
+                {
+                    Y = y - 1;
+                }
+                else Y--;
+            }
+            this.otvorenaUsta = !otvorenaUsta;
+        }
+
+        public void iscrtaj(Graphics g)
+        {
+            if (otvorenaUsta)
+            {
+                if(nasoka==Nasoka.desno)
+                    g.FillPie(brush, X* 40 + 10, Y* 40, RADIUS * 2, RADIUS * 2, 45, 270);
+                if (nasoka == Nasoka.dole)
+                    g.FillPie(brush, X * 40 + 10, Y * 40, RADIUS * 2, RADIUS * 2, 135, 270);
+                if (nasoka == Nasoka.gore)
+                    g.FillPie(brush, X * 40 + 10, Y * 40, RADIUS * 2, RADIUS * 2, 315, 270);
+                if (nasoka == Nasoka.levo)
+                    g.FillPie(brush, X * 40 + 10, Y * 40, RADIUS * 2, RADIUS * 2, 225, 270);
+
+            }
+            else
+            {
+                g.FillEllipse(brush, X* 40+10, Y* 40, RADIUS * 2, RADIUS * 2);
+            }
+        }
+
+    }
+}
